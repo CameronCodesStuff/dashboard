b45d55bc4a4cffe5d5f612c22ed713edcc8a6102
Cameron <detlaffcameron@gmail.com>
2026-06-21 21:33:23 +1200

    Update worker URL in CONFIG object

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   username: 'CameronCodesStuff',
-  worker: 'https://YOUR_WORKER_NAME.YOUR_SUBDOMAIN.workers.dev',
+  worker: 'https://dashboard.detlaffcameron.workers.dev',
   refreshInterval: 60,
   repoLimit: 30,
   commitFetchLimit: 12,
